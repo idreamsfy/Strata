42e6ee4fbef9c3c52b08893a9d68108cc7b9f7ee
Marc Henrard <marc@opengamma.com>
2015-11-02 16:52:26 +0000

    Start caldibration with FX. Added par spread sensitivity to pricer.

---
+++ modules/pricer/src/test/java/com/opengamma/strata/pricer/calibration/CalibrationZeroRateUsdEur2OisFxTest.java
@@ -0,0 +1,248 @@
+/**
+ * Copyright (C) 2015 - present by OpenGamma Inc. and the OpenGamma group of companies
+ *
+ * Please see distribution for license.
+ */
+package com.opengamma.strata.pricer.calibration;
+
+import static com.opengamma.strata.basics.currency.Currency.EUR;
+import static com.opengamma.strata.basics.currency.Currency.USD;
+import static com.opengamma.strata.basics.date.BusinessDayConventions.FOLLOWING;
+import static com.opengamma.strata.basics.date.DayCounts.ACT_360;
+import static com.opengamma.strata.basics.date.DayCounts.ACT_365F;
+import static com.opengamma.strata.basics.date.HolidayCalendars.EUTA;
+import static com.opengamma.strata.basics.date.HolidayCalendars.USNY;
+import static com.opengamma.strata.basics.index.OvernightIndices.USD_FED_FUND;
+import static com.opengamma.strata.finance.rate.swap.type.FixedOvernightSwapConventions.USD_FIXED_1Y_FED_FUND_OIS;
+import static org.testng.Assert.assertEquals;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.testng.annotations.Test;
+
+import com.opengamma.strata.basics.Trade;
+import com.opengamma.strata.basics.currency.Currency;
+import com.opengamma.strata.basics.currency.CurrencyAmount;
+import com.opengamma.strata.basics.currency.CurrencyPair;
+import com.opengamma.strata.basics.currency.FxMatrix;
+import com.opengamma.strata.basics.currency.MultiCurrencyAmount;
+import com.opengamma.strata.basics.date.BusinessDayAdjustment;
+import com.opengamma.strata.basics.date.DayCount;
+import com.opengamma.strata.basics.date.DaysAdjustment;
+import com.opengamma.strata.basics.date.HolidayCalendar;
+import com.opengamma.strata.basics.date.Tenor;
+import com.opengamma.strata.basics.index.Index;
+import com.opengamma.strata.basics.interpolator.CurveExtrapolator;
+import com.opengamma.strata.basics.interpolator.CurveInterpolator;
+import com.opengamma.strata.basics.market.ImmutableObservableValues;
+import com.opengamma.strata.basics.market.ObservableKey;
+import com.opengamma.strata.collect.id.StandardId;
+import com.opengamma.strata.collect.timeseries.LocalDateDoubleTimeSeries;
+import com.opengamma.strata.finance.fx.FxSwapConvention;
+import com.opengamma.strata.finance.fx.FxSwapTemplate;
+import com.opengamma.strata.finance.rate.deposit.TermDepositTrade;
+import com.opengamma.strata.finance.rate.deposit.type.ImmutableTermDepositConvention;
+import com.opengamma.strata.finance.rate.deposit.type.TermDepositConvention;
+import com.opengamma.strata.finance.rate.deposit.type.TermDepositTemplate;
+import com.opengamma.strata.finance.rate.swap.SwapTrade;
+import com.opengamma.strata.finance.rate.swap.type.FixedOvernightSwapTemplate;
+import com.opengamma.strata.market.curve.CurveGroupName;
+import com.opengamma.strata.market.curve.CurveName;
+import com.opengamma.strata.market.curve.definition.CurveGroupDefinition;
+import com.opengamma.strata.market.curve.definition.CurveNode;
+import com.opengamma.strata.market.curve.definition.FixedOvernightSwapCurveNode;
+import com.opengamma.strata.market.curve.definition.FxSwapCurveNode;
+import com.opengamma.strata.market.curve.definition.InterpolatedNodalCurveDefinition;
+import com.opengamma.strata.market.curve.definition.TermDepositCurveNode;
+import com.opengamma.strata.market.key.QuoteKey;
+import com.opengamma.strata.market.value.ValueType;
+import com.opengamma.strata.math.impl.interpolation.FlatExtrapolator1D;
+import com.opengamma.strata.math.impl.interpolation.LinearInterpolator1D;
+import com.opengamma.strata.pricer.rate.ImmutableRatesProvider;
+import com.opengamma.strata.pricer.rate.deposit.DiscountingTermDepositProductPricer;
+import com.opengamma.strata.pricer.rate.swap.DiscountingSwapProductPricer;
+
+/**
+ * Test for curve calibration in USD and EUR.
+ * The USD curve is obtained by OIS and the EUR one by FX Swaps from USD.
+ */
+@Test
+public class CalibrationZeroRateUsdEur2OisFxTest {
+
+  private static final LocalDate VALUATION_DATE = LocalDate.of(2015, 11, 2);
+
+  private static final CurveInterpolator INTERPOLATOR_LINEAR = new LinearInterpolator1D();
+  private static final CurveExtrapolator EXTRAPOLATOR_FLAT = new FlatExtrapolator1D();
+  private static final DayCount CURVE_DC = ACT_365F;
+  private static final LocalDateDoubleTimeSeries TS_EMTPY = LocalDateDoubleTimeSeries.empty();
+
+  private static final String SCHEME = "CALIBRATION";
+  private static final HolidayCalendar EUTA_USNY = EUTA.combineWith(USNY);
+
+  /** Curve names */
+  private static final String USD_DSCON_STR = "USD-DSCON-OIS";
+  private static final CurveName USD_DSCON_CURVE_NAME = CurveName.of(USD_DSCON_STR);
+  private static final String EUR_DSC_STR = "EUR-DSC-FX";
+  private static final CurveName EUR_DSC_CURVE_NAME = CurveName.of(EUR_DSC_STR);
+  /** Curves associations to currencies and indices. */
+  private static final Map<CurveName, Currency> DSC_NAMES = new HashMap<>();
+  private static final Map<CurveName, Set<Index>> IDX_NAMES = new HashMap<>();
+  private static final Map<Index, LocalDateDoubleTimeSeries> TS = new HashMap<>();
+  static {
+    DSC_NAMES.put(USD_DSCON_CURVE_NAME, USD);
+    Set<Index> usdFedFundSet = new HashSet<>();
+    usdFedFundSet.add(USD_FED_FUND);
+    IDX_NAMES.put(USD_DSCON_CURVE_NAME, usdFedFundSet);
+  }
+
+  /** Data FX **/
+  private static final double EUR_USD = 1.10;
+  private static final String EUR_USD_ID_VALUE = "EUR-USD";
+  /** Data for USD-DSCON curve */
+  /* Market values */
+  private static final double[] USD_DSC_MARKET_QUOTES = new double[] {
+    0.0016, 0.0022,
+    0.0013, 0.0016, 0.0020, 0.0026, 0.0033,
+    0.0039, 0.0053, 0.0066, 0.0090, 0.0111};
+  private static final int USD_DSC_NB_NODES = USD_DSC_MARKET_QUOTES.length;
+  private static final String[] USD_DSC_ID_VALUE = new String[] {
+    "USD-ON", "USD-TN",
+    "USD-OIS-1M", "USD-OIS-2M", "USD-OIS-3M", "USD-OIS-6M", "USD-OIS-9M",
+    "USD-OIS-1Y", "USD-OIS-18M", "USD-OIS-2Y", "USD-OIS-3Y", "USD-OIS-4Y"};
+  /* Nodes */
+  private static final CurveNode[] USD_DSC_NODES = new CurveNode[USD_DSC_NB_NODES];
+  /* Tenors */
+  private static final int[] USD_DSC_DEPO_OFFSET = new int[] {0, 1};
+  private static final int USD_DSC_NB_DEPO_NODES = USD_DSC_DEPO_OFFSET.length;  
+  private static final Period[] USD_DSC_OIS_TENORS = new Period[] {
+      Period.ofMonths(1), Period.ofMonths(2), Period.ofMonths(3), Period.ofMonths(6), Period.ofMonths(9),
+      Period.ofYears(1), Period.ofMonths(18), Period.ofYears(2), Period.ofYears(3), Period.ofYears(4)};
+  private static final int USD_DSC_NB_OIS_NODES = USD_DSC_OIS_TENORS.length;
+  static {
+    for(int i = 0; i < USD_DSC_NB_DEPO_NODES; i++) {
+      BusinessDayAdjustment bda = BusinessDayAdjustment.of(FOLLOWING, USNY);
+      TermDepositConvention convention = 
+          ImmutableTermDepositConvention.of(USD, bda, ACT_360, DaysAdjustment.ofBusinessDays(USD_DSC_DEPO_OFFSET[i], USNY));
+      USD_DSC_NODES[i] = TermDepositCurveNode.of(TermDepositTemplate.of(Period.ofDays(1), convention), 
+          QuoteKey.of(StandardId.of(SCHEME, USD_DSC_ID_VALUE[i])));
+    }
+    for (int i = 0; i < USD_DSC_NB_OIS_NODES; i++) {
+      USD_DSC_NODES[USD_DSC_NB_DEPO_NODES + i] = FixedOvernightSwapCurveNode.of(
+          FixedOvernightSwapTemplate.of(Period.ZERO, Tenor.of(USD_DSC_OIS_TENORS[i]), USD_FIXED_1Y_FED_FUND_OIS),
+          QuoteKey.of(StandardId.of(SCHEME, USD_DSC_ID_VALUE[USD_DSC_NB_DEPO_NODES + i])));
+    }
+  }
+  /** Data for EUR-DSC curve */
+  /* Market values */
+  private static final double[] EUR_DSC_MARKET_QUOTES = new double[] {
+    0.0004, 0.0012, 0.0019, 0.0043, 0.0074,
+    0.0109, 0.0193, 0.0294, 0.0519, 0.0757};
+  private static final int EUR_DSC_NB_NODES = EUR_DSC_MARKET_QUOTES.length;
+  private static final String[] EUR_DSC_ID_VALUE = new String[] {
+    "EUR-USD-FX-1M", "EUR-USD-FX-2M", "EUR-USD-FX-3M", "EUR-USD-FX-6M", "EUR-USD-FX-9M",
+    "EUR-USD-FX-1Y", "EUR-USD-FX-18M", "EUR-USD-FX-2Y", "EUR-USD-FX-3Y", "EUR-USD-FX-4Y"};
+  /* Nodes */
+  private static final CurveNode[] EUR_DSC_NODES = new CurveNode[EUR_DSC_NB_NODES];
+  /* Tenors */
+  private static final Period[] EUR_DSC_FX_TENORS = new Period[] {
+      Period.ofMonths(1), Period.ofMonths(2), Period.ofMonths(3), Period.ofMonths(6), Period.ofMonths(9),
+      Period.ofYears(1), Period.ofMonths(18), Period.ofYears(2), Period.ofYears(3), Period.ofYears(4)};
+  private static final int EUR_DSC_NB_FX_NODES = EUR_DSC_FX_TENORS.length;
+  static {
+    FxSwapConvention convention = 
+        FxSwapConvention.of(CurrencyPair.of(EUR, USD), DaysAdjustment.ofBusinessDays(2, EUTA_USNY));
+    for (int i = 0; i < EUR_DSC_NB_FX_NODES; i++) {
+      EUR_DSC_NODES[i] = FxSwapCurveNode.of(FxSwapTemplate.of(EUR_DSC_FX_TENORS[i], convention), 
+          QuoteKey.of(StandardId.of(SCHEME, EUR_USD_ID_VALUE)),
+          QuoteKey.of(StandardId.of(SCHEME, EUR_DSC_ID_VALUE[i])));
+    }
+  }
+
+  /** All quotes for the curve calibration */
+  private static final ImmutableObservableValues ALL_QUOTES;
+  static {
+    Map<ObservableKey, Double> map = new HashMap<>();
+    for (int i = 0; i < USD_DSC_NB_NODES; i++) {
+      map.put(QuoteKey.of(StandardId.of(SCHEME, USD_DSC_ID_VALUE[i])), USD_DSC_MARKET_QUOTES[i]);
+    }
+    for (int i = 0; i < EUR_DSC_NB_NODES; i++) {
+      map.put(QuoteKey.of(StandardId.of(SCHEME, EUR_DSC_ID_VALUE[i])), EUR_DSC_MARKET_QUOTES[i]);
+    }
+    map.put(QuoteKey.of(StandardId.of(SCHEME, EUR_USD_ID_VALUE)), EUR_USD);
+    ALL_QUOTES = ImmutableObservableValues.of(map);
+  }
+
+  private static final DiscountingSwapProductPricer SWAP_PRICER =
+      DiscountingSwapProductPricer.DEFAULT;
+  private static final DiscountingTermDepositProductPricer DEPO_PRICER =
+      DiscountingTermDepositProductPricer.DEFAULT;
+  
+  private static final CalibrationMeasures CALIBRATION_MEASURES = CalibrationMeasures.DEFAULT;
+  private static final CurveCalibrator CALIBRATOR = CurveCalibrator.of(1e-9, 1e-9, 100, CALIBRATION_MEASURES);
+
+  // Constants
+  private static final double TOLERANCE_PV = 1.0E-6;
+  private static final double TOLERANCE_PV_DELTA = 1.0E+3;
+
+  private static final CurveGroupName CURVE_GROUP_NAME = CurveGroupName.of("USD-DSCON-EUR-DSC");
+  private static final InterpolatedNodalCurveDefinition USD_DSC_CURVE_DEFN =
+      InterpolatedNodalCurveDefinition.builder()
+          .name(USD_DSCON_CURVE_NAME)
+          .xValueType(ValueType.YEAR_FRACTION)
+          .yValueType(ValueType.ZERO_RATE)
+          .dayCount(CURVE_DC)
+          .interpolator(INTERPOLATOR_LINEAR)
+          .extrapolatorLeft(EXTRAPOLATOR_FLAT)
+          .extrapolatorRight(EXTRAPOLATOR_FLAT)
+          .nodes(USD_DSC_NODES).build();
+  private static final InterpolatedNodalCurveDefinition EUR_DSC_CURVE_DEFN =
+      InterpolatedNodalCurveDefinition.builder()
+          .name(EUR_DSC_CURVE_NAME)
+          .xValueType(ValueType.YEAR_FRACTION)
+          .yValueType(ValueType.ZERO_RATE)
+          .dayCount(CURVE_DC)
+          .interpolator(INTERPOLATOR_LINEAR)
+          .extrapolatorLeft(EXTRAPOLATOR_FLAT)
+          .extrapolatorRight(EXTRAPOLATOR_FLAT)
+          .nodes(EUR_DSC_NODES).build();
+  private static final CurveGroupDefinition CURVE_GROUP_CONFIG =
+      CurveGroupDefinition.builder()
+          .name(CURVE_GROUP_NAME)
+          .addCurve(USD_DSC_CURVE_DEFN, USD, USD_FED_FUND)
+          .addDiscountCurve(EUR_DSC_CURVE_DEFN, EUR).build();
+  
+  //-------------------------------------------------------------------------
+  public void calibration_present_value_oneGroup() {
+    ImmutableRatesProvider result =
+        CALIBRATOR.calibrate(CURVE_GROUP_CONFIG, VALUATION_DATE, ALL_QUOTES, TS, FxMatrix.empty());
+    assertPresentValue(result);
+  }
+  
+  private void assertPresentValue(ImmutableRatesProvider result) {
+    // Test PV Dsc;
+    List<Trade> dscTrades = new ArrayList<>();
+    for (int i = 0; i < USD_DSC_NODES.length; i++) {
+      dscTrades.add(USD_DSC_NODES[i].trade(VALUATION_DATE, ALL_QUOTES));
+    }
+    // Depo
+    for (int i = 0; i < USD_DSC_NB_DEPO_NODES; i++) {
+      CurrencyAmount pvIrs = DEPO_PRICER
+          .presentValue(((TermDepositTrade) dscTrades.get(i)).getProduct(), result);
+      assertEquals(pvIrs.getAmount(), 0.0, TOLERANCE_PV);
+    }
+    // OIS
+    for (int i = 0; i < USD_DSC_NB_OIS_NODES; i++) {
+      MultiCurrencyAmount pvIrs = SWAP_PRICER
+          .presentValue(((SwapTrade) dscTrades.get(USD_DSC_NB_DEPO_NODES + i)).getProduct(), result);
+      assertEquals(pvIrs.getAmount(USD).getAmount(), 0.0, TOLERANCE_PV);
+    }
+  }
+
+}
